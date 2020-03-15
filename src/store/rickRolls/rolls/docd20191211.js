0e00c88cf06c5db37a1c4080e645e467930e6a4a
docd27 <58469465+docd27@users.noreply.github.com>
2020-03-15 00:00:04 +0000

    More docd27 RickRolls

---
+++ src/store/rickRolls/rolls/docd20191211.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: 'docd27',
+    url: 'https://www.twitch.tv/docd27',
+  },
+  type: 'Youtube',
+  url: 'https://www.youtube.com/embed/EnfLqIm3dDg?start=1034',
+  date: new Date('2019-12-11'),
+  originalUrl: 'https://www.youtube.com/watch?v=EnfLqIm3dDg&t=1034',
+};
