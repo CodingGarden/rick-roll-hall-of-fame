efa675079fd1da14ce8faaf0f23f9568af4ede9c
docd27 <58469465+docd27@users.noreply.github.com>
2020-03-15 00:27:28 +0000

    docd27 2020-03-14

---
+++ src/store/rickRolls/rolls/rickastley19870727.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: 'Richard Paul Astley',
+    url: 'https://en.wikipedia.org/wiki/Rick_Astley',
+  },
+  type: 'Video',
+  url: 'https://archive.org/serve/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4',
+  date: new Date('1987-07-27'),
+  originalUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+};
