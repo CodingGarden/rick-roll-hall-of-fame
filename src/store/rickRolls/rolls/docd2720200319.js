b2fec8f1a915e4a1b991b0b788c860da40f90f07
docd27 <58469465+docd27@users.noreply.github.com>
2020-04-15 06:19:12 +0100

    docd27 2020-03-19

---
+++ src/store/rickRolls/rolls/docd2720200319.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: 'docd27',
+    url: 'https://www.twitch.tv/docd27',
+  },
+  type: 'Twitch',
+  url: 'https://clips-media-assets2.twitch.tv/AT-cm%7C644139816.mp4',
+  date: new Date('2020-03-19'),
+  originalUrl: 'https://www.youtube.com/watch?v=JNagbi_QvIU&t=893',
+};
