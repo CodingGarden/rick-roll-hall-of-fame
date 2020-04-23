f0a92d1dbb75899b955592d595a15f2bb3cb7535
Brandon <brandonstrash91@yahoo.com>
2020-04-23 08:42:43 -0400

    add the two latest rick rolls

---
+++ src/store/rickRolls/rolls/amrB9320200422.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: "amrBn3",
+    url: "https://www.twitch.tv/amrb93",
+  },
+  type: "Twitch",
+  url: "https://clips-media-assets2.twitch.tv/AT-cm%7C682769569.mp4",
+  date: new Date("2020-04-22"),
+  originalUrl: "https://www.youtube.com/watch?v=5aaSxxgEYj0",
+};
---
+++ src/store/rickRolls/rolls/shaggieh20200422.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: "shaggieh",
+    url: "https://www.twitch.tv/shaggieh",
+  },
+  type: "Twitch",
+  url: "https://clips-media-assets2.twitch.tv/AT-cm%7C682767527.mp4",
+  date: new Date("2020-04-22"),
+  originalUrl: "https://www.youtube.com/watch?v=5aaSxxgEYj0",
+};
