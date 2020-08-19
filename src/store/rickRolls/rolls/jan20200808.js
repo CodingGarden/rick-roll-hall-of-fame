6175c89385fa834b59a43827057ef30fb50782c2
Leon <leonschneider29@googlemail.com>
2020-08-19 17:02:09 +0200

    Create jan20200808.js
    added rickroll by jaany from 8th August at the drop game stream

---
+++ src/store/rickRolls/rolls/jan20200808.js
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    username: "jaany",
+    url: "https://www.twitch.tv/jaany",
+  },
+  type: "Twitch",
+  url: "https://clips-media-assets2.twitch.tv/AT-cm%7C827442189.mp4",
+  date: new Date("2020-08-08"),
+  originalUrl: "https://www.youtube.com/watch?v=zvMhK5U0KIw",
+};
